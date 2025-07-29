99779e797303fc96f0981f4cdb290d714fa75645
Sinchana08 <naik.sinchana13@gmail.com>
2025-07-29 19:14:39 +0530

    workbench codes

---
+++ 14_hibernate/workbench2.sql
@@ -0,0 +1,13 @@
+use neon;
+
+show tables;
+drop table student_laptop;
+drop table laptop_student;
+drop table student;
+drop table laptop;
+select * from student_laptop;
+select * from laptop_student;
+select * from laptop;
+select * from student;
+DELETE FROM Laptop WHERE lid = 101;
+DELETE FROM student WHERE Rollno = 1;
