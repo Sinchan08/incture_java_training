2c67f2f9b869c7ccb8e25ffb4958d6c76d98f3d2
Sinchana08 <naik.sinchana13@gmail.com>
2025-08-06 10:06:59 +0530

    codes

--- 19_JUnit/Junit_Codes/CalculatorTest.java
+++ 19_JUnit/Junit_Codes/CalculatorTest.java
@@ -2,6 +2,10 @@ package com.example.first_springboot;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
@@ -15,6 +19,7 @@ class CalculatorTest {
 		int actual_result=cal.add(10,20);
 		int expected_result=30;
 		assertEquals(expected_result,actual_result);
+		System.out.println("Test Case:"+"testAdd");
 		
 	}
 	
@@ -23,24 +28,54 @@ class CalculatorTest {
 		cal=new Calculator();
 		int actual_result=cal.sub(200,100);
 		int expected_result=100;
-		assertEquals(expected_result,actual_result);
+		// assertEquals(expected_result,actual_result);
+		assertTrue(actual_result==expected_result);
+		System.out.println("Test Case:"+"testSub");
 		
 	}
-	@Test
-	void testMul() {
-		cal=new Calculator();
-		int actual_result=cal.mul(20,10);
-		int expected_result=300;
-		assertEquals(expected_result,actual_result);
-		
+//	@Test
+//	void testMul() {
+//		cal=new Calculator();
+//		int actual_result=cal.mul(20,10);
+//		int expected_result=300;
+//		assertEquals(expected_result,actual_result);
+//		
+//	}
+//	@Test
+//	void testDiv() {
+//		cal=new Calculator();
+//		int actual_result=cal.div(200,100);
+//		int expected_result=2;
+//		assertEquals(expected_result,actual_result);
+//		
+//	}
+//	@Test
+//	void testGiveObj() {
+//		cal=new Calculator();
+//		//assertNotNull(cal.giveObj()); //test case pass
+//		assertNull(cal.giveObj());       //fail
+//	}
+	
+	@BeforeEach
+	public void m1()
+	{
+		System.out.println("Our Test Case To BE Started.........");
 	}
-	@Test
-	void testDiv() {
-		cal=new Calculator();
-		int actual_result=cal.div(200,100);
-		int expected_result=2;
-		assertEquals(expected_result,actual_result);
-		
+	
+	@AfterEach
+	public void m2()
+	{
+		System.out.println("Our Test Case Completed............");
+	}
+	
+	@BeforeAll                                            
+	public  static void m3() {                                  //it will give error if it is not static
+		System.out.println("Before All Test Cases");
+	}
+	
+	@AfterAll
+	public static void m4() {
+		System.out.println("After All Test Case");
 	}
 
 }
