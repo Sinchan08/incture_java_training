f4475064df1cb05d6084916937ddf695bb905eca
Sinchana08 <naik.sinchana13@gmail.com>
2025-09-08 18:15:49 +0530

    html and css

---
+++ React/Javascript/First.js
@@ -0,0 +1,43 @@
+//console.log("welcome to react");
+//alert("welcome to Java Script Session...")
+
+
+//var x = 10;
+//let x=10;
+//console.log("X:",x);
+//x=100;
+//console.log("X:",x);
+
+//if(true)
+//{
+    //var x=10;
+    //let x=10;
+    //const x=10;
+    //console.log("X:",x);
+   // const x=100;
+   // console.log("X:",x);
+//}
+//console.log("X:",x);
+
+var x=10;
+console.log("X:",x);
+
+var x=200;    // redeclaration initializing same variable again
+console.log("X:",x);
+
+let y=10;
+console.log("Y:",y);
+
+x="ABC";
+console.log("X:",x);
+console.log(typeof x);
+
+let a=10;
+let b="10";
+console.log(typeof a);
+console.log(typeof b);
+console.log(b==a);
+console.log(b===a);
+
+
+
---
+++ React/Javascript/Second.js
@@ -0,0 +1,10 @@
+let age=18;
+//if(age>=18)
+//{
+   // console.log("You are eligible");
+//}
+//else{
+    //console.log("Not eligible");
+//}
+
+age>=18 ? console.log("Elligible"): console.log("Not eligible")
